a5982dd94949f2785b4e68d164f7cafa3dbd8ef2
Viktor Filippov <Mozgfoxtrot@gmail.com>
2023-02-07 18:49:24 +0400

    fixed HTML + width(profile) + JS

--- scripts/app.js
+++ scripts/app.js
@@ -2,27 +2,25 @@ const editBtn = document.querySelector(".profile__edit-button");
 const closeBtn = document.querySelector(".popup__close-button");
 const popup = document.querySelector(".popup");
 const popupForm = document.querySelector(".popup__form");
-const inputName = document.querySelector(".popup__input-name");
-const inputAbout = document.querySelector(".popup__input-about");
+const inputName = document.querySelector("#name");
+const inputAbout = document.querySelector("#about");
 const title = document.querySelector(".profile__title");
 const subtitle = document.querySelector(".profile__subtitle");
-const saveBtn = document.querySelector(".popup__save-button");
 
 function togglePopup() {
   popup.classList.toggle("popup_open");
+  inputName.value = title.textContent;
+  inputAbout.value = subtitle.textContent;
 }
 
 editBtn.addEventListener("click", togglePopup);
 closeBtn.addEventListener("click", togglePopup);
-saveBtn.addEventListener("click", togglePopup);
-
-inputName.value = title.textContent;
-inputAbout.value = subtitle.textContent;
 
 function handleFormSubmit(evt) {
   evt.preventDefault();
   title.textContent = inputName.value;
   subtitle.textContent = inputAbout.value;
+  togglePopup();
 }
 
 popupForm.addEventListener("submit", handleFormSubmit);
